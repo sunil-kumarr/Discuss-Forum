d2d2f020def12c05debef2a985eca88cf8f15ac4
sunil kumar <sunindus.sk@gmail.com>
2020-02-18 22:24:03 +0530

    added posts used for loop in jinja2

--- app/routes.py
+++ app/routes.py
@@ -13,5 +13,14 @@ from flask import render_template
 @app.route("/index")
 def index():
     user = {'username':'sunil'}
-
-    return render_template('index.html',title='Home',user = user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',title='Home',user = user, posts=posts)
