72d919de6148b67fc0864f4d8614c1c5088d3e30
sunil kumar <sunindus.sk@gmail.com>
2020-02-18 23:38:12 +0530

    added form validation

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ The render_template() function invokes the Jinja2 template engine that comes bun
 with the Flask framework. Jinja2 substitutes {{ ... }} blocks with the corresponding
  values, given by the arguments provided in the render_template() call.
 '''
-from flask import render_template
+from flask import render_template,flash,redirect
 from app.forms import LoginForm
 
 #index function return data against this to urls defined as decorater function
@@ -26,7 +26,11 @@ def index():
     ]
     return render_template('index.html',title='Home',user = user, posts=posts)
 
-@app.route("/login")
+@app.route("/login" ,methods = ['GET','POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            login_form.username.data, login_form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',title='Sign In',form=login_form)
