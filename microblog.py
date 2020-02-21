edb3e31088a775cb42767aaf46dc9845386fe24d
sunil kumar <sunindus.sk@gmail.com>
2020-02-21 23:44:26 +0530

    migration scripts and db file

--- microblog.py
+++ microblog.py
@@ -1,4 +1,9 @@
 """
  pip install python-dotenv // for auto setting export FLASK_APP = microblog.py
 """
-from app import app
+from app import app,db
+from app.models import User,Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
