f1887f8499ad788d58f269c72ca24b9de318814b
sunil kumar <sunindus.sk@gmail.com>
2020-02-21 22:54:13 +0530

    added flask-sqlalchemy & migrate in init.py,config

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+basedir =  os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     # get the secret-key to be used by flask-wtf to safeguard from CSRF attacks from system veriables or use default
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'user-will-never-guess-it'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
