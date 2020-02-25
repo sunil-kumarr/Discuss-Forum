a7eceab39b4eb60d707fc26a91025845251543f4
sunil kumar <sunindus.sk@gmail.com>
2020-02-25 19:57:54 +0530

    making login required

--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,16 @@ The render_template() function invokes the Jinja2 template engine that comes bun
 with the Flask framework. Jinja2 substitutes {{ ... }} blocks with the corresponding
  values, given by the arguments provided in the render_template() call.
 '''
-from flask import render_template,flash,redirect,url_for
+from flask import render_template,flash,redirect,url_for,request
 from app.forms import LoginForm
-from flask_login import current_user,login_user,logout_user
+from flask_login import current_user,login_user,logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 #index function return data against this to urls defined as decorater function
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     user = {'username':'sunil'}
     posts = [
@@ -39,7 +41,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user,remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc !='':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',title='Sign In',form = form)
 
 @app.route('/logout')
