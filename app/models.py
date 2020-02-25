4a28b3dd210fafd34040424d45142738ed91a682
sunil kumar <sunindus.sk@gmail.com>
2020-02-25 20:06:42 +0530

    fixed fucntion

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self,password):
-        return check_password(password,self.password_hash)
+        return check_password_hash(password,self.password_hash)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
