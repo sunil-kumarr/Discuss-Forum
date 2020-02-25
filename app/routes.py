1cbaa5faacbbebb94f1afb525cc245e81a18a899
sunil kumar <sunindus.sk@gmail.com>
2020-02-25 19:49:55 +0530

    login user route update

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,8 @@ with the Flask framework. Jinja2 substitutes {{ ... }} blocks with the correspon
 '''
 from flask import render_template,flash,redirect,url_for
 from app.forms import LoginForm
+from flask_login import current_user,login_user
+from app.models import User
 
 #index function return data against this to urls defined as decorater function
 @app.route("/")
@@ -28,9 +30,15 @@ def index():
 
 @app.route("/login" ,methods = ['GET','POST'])
 def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            login_form.username.data, login_form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html',title='Sign In',form=login_form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user,remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html',title='Sign In',form = form)
+
