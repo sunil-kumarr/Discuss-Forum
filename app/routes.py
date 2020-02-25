3b574f1dfa1662fffbffcfbc0a5d733915b523ce
sunil kumar <sunindus.sk@gmail.com>
2020-02-25 20:28:40 +0530

    added route for register

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 #flask app instance created in __init__.py in app package
-from app import app
+from app import app,db
 '''
 To render HTML page
 The render_template() function invokes the Jinja2 template engine that comes bundled 
@@ -7,7 +7,7 @@ with the Flask framework. Jinja2 substitutes {{ ... }} blocks with the correspon
  values, given by the arguments provided in the render_template() call.
 '''
 from flask import render_template,flash,redirect,url_for,request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user,login_user,logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -50,3 +50,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
