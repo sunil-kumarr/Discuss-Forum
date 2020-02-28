f22f68688fb3fd1d7273983ad8d6c5e9d05229c5
sunil kumar <sunindus.sk@gmail.com>
2020-02-28 22:03:51 +0530

    added edit_profile feature

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,BooleanField,PasswordField,SubmitField
-from wtforms.validators import DataRequired,ValidationError,Email,EqualTo
+from wtforms import StringField,BooleanField,PasswordField,SubmitField,TextAreaField
+from wtforms.validators import DataRequired,ValidationError,Email,EqualTo,Length
 from app.models import User
 
 
@@ -23,8 +23,14 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username = username.data).first()
         if user is not None:
             raise ValidationError('Username not available!')
-    
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Email already used!')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
