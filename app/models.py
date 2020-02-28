910cf9e3dc2442326ba681335f4bb2a7487177d0
sunil kumar <sunindus.sk@gmail.com>
2020-02-28 20:25:52 +0530

    added gravatar for user profile

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from app import db,login
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -17,6 +19,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
