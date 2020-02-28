032d195c8f6b983eb5d5e7497a095e1a04e3597e
sunil kumar <sunindus.sk@gmail.com>
2020-02-28 22:31:30 +0530

    added error handling,email,logging feature

--- microblog.py
+++ microblog.py
@@ -1,7 +1,7 @@
 """
  pip install python-dotenv // for auto setting export FLASK_APP = microblog.py
 """
-from app import app,db
+from app import app, db
 from app.models import User,Post
 
 @app.shell_context_processor
